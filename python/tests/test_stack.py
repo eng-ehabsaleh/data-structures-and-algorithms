4ead38c711e449e8f7f60e0e261dca0c1dd6bce0
ehab-ahma <ehab24495@gmail.com>
2021-11-06 15:04:58 +0200

    done with fixing code challenge 16

--- python/tests/test_stack.py
+++ python/tests/test_stack.py
@@ -1,41 +1,41 @@
-# from stack_and_queue.node import Node
-# from stack_and_queue.stack import Stack
-# import pytest
+from stack_and_queue.node import Node
+from stack_and_queue.stack import Stack
+import pytest
 
 
-# def test_push(stack):
-#     """
-#     """
-#     actual = stack.top.value
-#     expected = "cat"
-#     assert actual == expected
+def test_push(stack):
+    """
+    """
+    actual = stack.top.value
+    expected = "cat"
+    assert actual == expected
 
 
-# def test_pop(stack):
-#     """
-#     """
-#     actual = stack.pop()
-#     expected = "cat"
-#     assert actual == expected
+def test_pop(stack):
+    """
+    """
+    actual = stack.pop()
+    expected = "cat"
+    assert actual == expected
 
 
-# def test_is_empty(stack):
-#     assert stack.is_empty() == True
+def test_is_empty(stack):
+    assert stack.is_empty() == True
 
 
-# def test_peek(stack):
-#     actual = stack.peek()
-#     expected = 'cat'
-#     assert actual == expected
+def test_peek(stack):
+    actual = stack.peek()
+    expected = 'cat'
+    assert actual == expected
 
-# # decorator
+# decorator
 
 
-# @pytest.fixture
-# def stack():
-#     stack = Stack()
-#     stack.push(1)
-#     stack.push(2)
-#     stack.push("cat")
+@pytest.fixture
+def stack():
+    stack = Stack()
+    stack.push(1)
+    stack.push(2)
+    stack.push("cat")
 
-#     return stack
+    return stack
